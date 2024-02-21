5361c437915684a30a022c25354373f37c3b0aec
PM <121531128+26-PM@users.noreply.github.com>
2024-02-21 10:20:08 +0000

    divided into 2 part

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
 import Navbar from "./components/Navbar"
 import { v4 as uuidv4 } from 'uuid';
-import { useState } from "react"
+import { useState,useEffect } from "react"
 function App() {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    let todoString=localStorage.getItem ("todos" );
+    if (todoString){
+      let todos= JSON.parse(localStorage.getItem ("todos" ))
+      setTodos(todos);  
+    }
+  }, [])
+  
+  const saveToLS=( ) => {
+    localStorage.setItem("todos",JSON.stringify (todos))
+  }
+  
   const handleAdd=()=>{
     setTodos([...todos,{id:uuidv4(), todo , isCompleted:false}])
     setTodo("")
+    saveToLS();
   }
+
   const handleEdit=(e,id )=>{
     let t=todos.filter(i=>i.id===id)
     setTodo(t[0].todo) 
     let newTodos=todos.filter(item=>{
       return item.id!==id 
-     });
-     setTodos(newTodos)  
-     
+    });
+    setTodos(newTodos)  
+    saveToLS();
   }
+
   const handleDelete=(e,id)=>{
-     let newTodos=todos.filter(item=>{
+    let newTodos=todos.filter(item=>{
       return item.id!==id 
-     });
-     setTodos(newTodos) 
+    });
+    setTodos(newTodos) 
+    saveToLS();
   }
+
   const handleChange=(e)=>{
     setTodo(e.target.value)
   }
+
   const handleCheckbox=(e) => {
      let id=e.target.name;
      let index=todos.findIndex(item=>{
@@ -34,18 +53,21 @@ function App() {
      let newTodos=[...todos];
      newTodos[index].isCompleted=!newTodos[index].isCompleted
      setTodos(newTodos)
+     saveToLS();
   }
   
   return (
     <>
     <Navbar/>
-    <div className="container my-5 mx-auto rounded-xl p-5 bg-black text-white min-h-[83vh]">
+    <div className="container my-5 mx-auto rounded-xl p-5 bg-black text-white">
       <div className="addTodo">
-        <h2 className="text-lg">Add todo</h2>
+        <h2 className="text-xl font-black">Add todo</h2>
         <input onChange={handleChange} value={todo } className="text-black" type="text" />
         <button onClick={handleAdd} className="hover:font-bold text-md bg-white text-black mx-1 p-3 py-1 rounded-md hover:text-bold">Save</button>
       </div>
-      <h2 className="text-lg my-5">Your Todos</h2> 
+      </div>
+      <div className="yourTodos container my-5 mx-auto rounded-xl p-5 bg-black text-white">
+      <h2 className="text-xl font-black my-5">Your Todos</h2> 
       <div className="todos">
         {todos.length===0 && <div className="m-5">No todos</div>}
         {todos.map(item=>{
@@ -61,7 +83,7 @@ function App() {
       </div>
       })}
       </div>
-    </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='mx-1 my-5 navbar flex justify-between bg-black text-white p-4 px-20 h-16 text-xl'>
+    <div className='mx-1 my-5 navbar flex justify-evenly items-center bg-black text-white p-4 px-20 h-20 text-xl'>
         <div className="logo cursor-pointer hover:text-lg transition-all duration-5 hover:font-bold">Task Manager</div>
       <ul className='flex gap-10'>
         <li className='cursor-pointer hover:text-lg transition-all duration-5 hover:font-bold'>Home</li>
